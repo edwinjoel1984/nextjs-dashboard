0622b3a6a483c0836419cfccadcf872a6d90cefd
Edwin Ulloa <edwinjoel1984@gmail.com>
2025-02-04 18:40:27 -0500

    Add all files completed course

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,12 +14,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -213,5 +213,16 @@ export async function fetchFilteredCustomers(query: string) {
   } catch (err) {
     console.error('Database Error:', err);
     throw new Error('Failed to fetch customer table.');
+  }
+}
+
+export async function getJsonPlaceholderData() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Failed to fetch JSONPlaceholder data:', error);
+    throw new Error('Failed to fetch JSONPlaceholder data.');
   }
 }
